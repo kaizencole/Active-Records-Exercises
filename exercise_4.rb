11121da326c51822cd992060d555af1c8b5b9993
kaizencole <papertigerlily@gmail.com>
2016-06-08 18:17:53 +0000

    Active Record exercises

--- exercise_4.rb
+++ exercise_4.rb
@@ -2,8 +2,24 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
+#require 'pry-byebug'
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+Store.where(mens_apparel: true).each do |store|
+#.find_each do |store|
+  #binding.pry
+  print store.name + " "
+  puts store.annual_revenue
+end
+
+Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000).each do |store|
+  print store.name + " "
+  puts store.annual_revenue
+end
