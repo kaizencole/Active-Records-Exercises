b1acd87f74a3c0ffd7ad3f067e71c4ea1c0eefea
kaizencole <papertigerlily@gmail.com>
2016-06-08 17:11:41 +0000

    Active Record Exercises

---
+++ exercise_1.rb
@@ -0,0 +1,12 @@
+require_relative '../setup'
+
+puts "Exercise 1"
+puts "----------"
+
+# Your code goes below here ...
+
+Store.create(name: 'Burnaby', annual_revenue: 300000)
+Store.create(name: 'Richmond', annual_revenue: 1260000)
+Store.create(name: 'Gastown', annual_revenue: 190000)
+
+puts Store.count
